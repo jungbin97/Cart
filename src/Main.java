403f15d896e7eadad953f7d67afcd1d503adb661
jungbin97 <ljb5054@gmail.com>
2023-10-05 15:33:30 +0900

    feat : i/o스트림을 사용하여 csv파일로부터 상품목록 불러 오기

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
-import java.lang.reflect.Member;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // 상품 목록 생성
-        HashSet<Product> set = new HashSet<>();
+        // 상품 목록 생성(csv파일에 로드 해옴)
+        HashSet<Product> set = loadProdct("src/상품품목.csv");
 
         // TODO: 상품 클래스를 생성하여 상품목록에 넣는다.
         Product milk = new Product("우유", 1000);
@@ -45,5 +48,38 @@ public class Main {
 
         // 장바구니에 없는 상품 삭제 시 예외처리
         cart.removeProduct(cigarette, 2);
+    }
+
+
+public static HashSet<Product> loadProdct(String path){
+    BufferedReader br = null;
+    HashSet<Product> set = new HashSet<>();
+    try {
+        br = Files.newBufferedReader(Paths.get(path));
+        String line = "";
+
+        while((line = br.readLine()) != null){
+            // CSV 1행을 저장하는 리스트
+            String[] data = line.split(",");
+            // 배열로 받은 name, price
+            String name = data[0];
+            int price = Integer.parseInt(data[1]);
+
+            set.add(new Product(name, price));
+        }
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {   // 입출력 예외 처리
+        throw new RuntimeException(e);
+    } finally {
+        try {
+            if(br!= null){
+                br.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    return set;
     }
 }
